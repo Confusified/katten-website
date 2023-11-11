1ffc0ac5525892550aeb2c3a097aabb555abd254
Confusified <60018584+Confusified@users.noreply.github.com>
2023-11-11 01:19:25 +0100

    fix hannah.html image gallery

--- scripts.js
+++ scripts.js
@@ -17,6 +17,9 @@ function onLoad(){
 		text = text.replace("%%",currentAge); //Vervang %% met de leeftijd
 		aboutMeParagraph.innerHTML = text;
 	}
+	else if (typeof document.getElementById("hannahParagraph") !== "undefined" && document.getElementById("hannahParagraph") !== null) { //onload script voor hannah.html
+		showSlide(slideIndex = 1);
+	}
 }
 window.onload = onLoad;
 
@@ -31,4 +34,20 @@ function toggleAboutMe(){
 		container.style.display = "block";
 		button.innerHTML = "Klik hier om niet meer over mij te lezen";
 	}
+}
+
+function showSlide(n) {
+  let i;
+  const slides = document.getElementsByClassName("slideFade");
+  const dots = document.getElementsByClassName("imageDot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
 }
